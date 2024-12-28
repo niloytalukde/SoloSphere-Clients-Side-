205ca18c5b908fd0a2f0f4ef985923ec6842eeca
niloytalukde <niloytalukderme@gmail.com>
2024-12-28 12:02:59 +0530

    Added All Things

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwgUnrcAVESIoMD4MaZWtBStw1VXSLSPI",
+  authDomain:  "solo-sphere-59247.firebaseapp.com",
+  projectId:  "solo-sphere-59247",
+  storageBucket: "solo-sphere-59247.firebasestorage.app",
+  messagingSenderId: "663621982596",
+  appId: "1:663621982596:web:6f963973186b0eb43065d1",
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
